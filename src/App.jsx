cd7e008a8519d5aee6301d944d52edd413dea1ee
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-04-02 19:04:06 -0500

    Se agrega el Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import InserPasswordForm from './screens/views/InsertPasswordForm'
+
+function App() {
+
+  return (
+    <InserPasswordForm/>
+  )
+}
+
+export default App
---
+++ src/screens/views/InsertPasswordForm.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const InserPasswordForm = () => {
+  
+    //Aqui Ingresar Logica
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default InserPasswordForm;
