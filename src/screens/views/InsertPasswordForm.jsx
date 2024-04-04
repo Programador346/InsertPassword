2f47509aae06f45c5d78a62e3431416117038f12
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-04-03 20:19:27 -0500

    Diseño
    Cambio de icono y titulo de la Pestaña

--- src/screens/views/InsertPasswordForm.jsx
+++ src/screens/views/InsertPasswordForm.jsx
@@ -11,7 +11,7 @@ const InserPasswordForm = () => {
       <div className="mx-auto">
         <div className="flex justify-center items-center h-screen" style={{ backgroundImage: 'url("https://www.ikusi.com/mx/wp-content/uploads/sites/2/2022/12/ikusi_ikusi_image_403.jpg")', backgroundSize: 'cover' }}>
           <div className="bg-black bg-opacity-75 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <h2 className="text-center text-3xl font-serif mb-4 text-white">Bienvenido a la Plataforma el siguiente paso es crear tu Contraseña</h2>
+            <h2 className="text-center text-3xl font-serif mb-4 text-white">Bienvenido a la Plataforma el siguiente paso es cambiar tu Contraseña</h2>
             <form>
               <input
                 className="font-serif block w-full bg-transparent text-white border-b-2 border-gray-500 py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-blue-500 transition duration-500 ease-in-out transform hover:scale-105"
@@ -22,7 +22,7 @@ const InserPasswordForm = () => {
                 className="w-full bg-blue-500 bg-opacity-75 hover:bg-opacity-100 text-white font-serif py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-500 ease-in-out transform hover:scale-105"
                 type="submit"
               >
-                Crear Contraseña
+                Cambiar Contraseña
               </button>
             </form>
           </div>
